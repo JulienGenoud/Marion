c5ec01fcc0aedd9edbc05971b284d6ee42f03a3d
Julien <juliengenoud@gmail.com>
2016-04-21 05:30:07 +0800

    force render work page when  /

--- js/main.js
+++ js/main.js
@@ -32,30 +32,33 @@ scotchApp.config(function($routeProvider) {
 // create the controller and inject Angular's $scope
 scotchApp.controller('mainController', function($scope) {
   // create a message to display in our view
-  $scope.message = 'Everyone come and see how good I look!';
+  // $scope.message = 'Everyone come and see how good I look!'
+
+  // Force to render work page
+  location.href = "#work";
+
   // $(".rslides").responsiveSlides();
-  $("#home").addClass('active');
-  $("#work").removeClass('active');
+  $("#work").addClass('active');
+  $("#home").removeClass('active');
   $("#about").removeClass('active');
   $("#contact").removeClass('active');
 
-  $(document).ready(function(){
-       $('.your-class').slick({
-          arrows: true,
-         //
-        // dots: true,
-           infinite: true,
-           speed: 500,
-           cssEase: 'linear',
-           slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 3000,
-          });
-     });
+  // $(document).ready(function(){
+  //      $('.your-class').slick({
+  //         arrows: true,
+  //        //
+  //       // dots: true,
+  //          infinite: true,
+  //          speed: 500,
+  //          cssEase: 'linear',
+  //          slidesToScroll: 1,
+  //         autoplay: true,
+  //         autoplaySpeed: 3000,
+  //         });
+  //    });
 });
 
 scotchApp.controller('aboutController', function($scope) {
-
   $("#home").removeClass('active');
   $("#work").removeClass('active');
   $("#about").addClass('active');
